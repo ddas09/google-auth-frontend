def5e9cf396ae9f9ea160516d686f650eca28fd5
ddas09 <dipankar62918@gmail.com>
2021-07-11 11:06:35 +0530

    made reusable functions and observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   isLoggedIn: boolean = false;
   idToken: string = '';
   errorMsg: string = '';
+  errorCode?: number;
 
   constructor(
     private googleService: SocialAuthService,
@@ -24,94 +25,101 @@ export class AppComponent {
   // when sign in button clicked
   async onSignIn() {
     await this.getIdToken();
-    this.loginUser();
+    await this.loginUser();
+    if (this.errorCode !== undefined) {
+      if (this.errorCode === 606) {
+        const prompt: string = 
+        `Your google account is not registered with us!\n
+        Do you want to sign up for a new account?`;
+        if (this.getUserConfirmation(prompt))
+        {
+          this.registerUser();
+        } else {
+          this.logoutUser();
+        }
+      } 
+      this.autoHideErrorMsg();
+    }
   }
 
   // when sign up button clicked
   async onSignUp() {
     await this.getIdToken();
-    this.registerUser();
+    await this.registerUser();
+    if (this.errorCode !== undefined) {
+      if (this.errorCode === 606) {
+        const prompt: string = 
+        `Your google account is already registered with us!\n
+        Do you want to log in to your existing account?`; 
+        if (this.getUserConfirmation(prompt))
+        {
+          this.loginUser();
+        } else {
+          this.logoutUser();
+        }
+      } 
+      this.autoHideErrorMsg();
+    }
   }
 
-  // get id token from google 
   async getIdToken() {
     await this.googleService.signIn(GoogleLoginProvider.PROVIDER_ID);
     await this.googleService.authState.pipe(first()).toPromise()
       .then((user) => this.idToken = user.idToken)
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
+  }
+
+  // observer for login / register
+  private authObserver = {
+    next: (user: User) => {
+      this.user = user;
+      this.isLoggedIn = true;
+    },
+    error: (err: any) => {
+      if ("errorCode" in err) {
+        this.errorCode = err.errorCode;
+        if (err.errorCode !== 606) {
+          this.errorMsg = err.message;
+        }
+      } else {
+        this.errorCode = 500;
+        this.errorMsg = "INTERNAL SERVER ERROR";
+      }
+    }
   }
 
   // verify id token in the backend to authenticate user
-  loginUser(): void {
-    this.authService.loginUser(this.idToken).subscribe(
-      (user: User) => {
-        this.user = user
-        this.isLoggedIn = true;
-      },
-      (err) => {
-        if ("errorCode" in err) {
-          if (err.errorCode === 606) {
-            const promptMsg: string = `
-            Your google account is not registered with us!\n
-            Do you want to sign up for a new account?`;
-            let confirmation = window.confirm(promptMsg);
-            if (confirmation) {
-              this.registerUser();
-            } else {
-              this.logoutUser();
-            }
-          } else {
-            this.errorMsg = `${err.errorCode}: ${err.message}`.toUpperCase();
-          }
-        } else {
-          this.errorMsg = "500: INTERNAL SERVER ERROR";
-        }
-        setTimeout(() => {
-          this.errorMsg = '';
-        }, 3000)
-      }
-    );
+  async loginUser() {
+    await this.authService.loginUser(this.idToken).toPromise()
+      .then(this.authObserver.next)
+      .catch(this.authObserver.error);
   }
 
-  registerUser(): void {
-    this.authService.registerUser(this.idToken).subscribe(
-      (user: User) => {
-        this.user = user
-        this.isLoggedIn = true;
-      },
-      (err) => {
-        if ("errorCode" in err) {
-          if (err.errorCode === 606) {
-            const promptMsg: string = `
-            Your google account is already registered with us!\n
-            Do you want to log in to your exsisting account?`;
-            let confirmation = window.confirm(promptMsg);
-            if (confirmation) {
-              this.loginUser();
-            } else {
-              this.logoutUser();
-            }
-          } else {
-            this.errorMsg = `${err.errorCode}: ${err.message}`.toUpperCase();
-          }
-        } else {
-          this.errorMsg = "500: INTERNAL SERVER ERROR";
-        }
-        setTimeout(() => {
-          this.errorMsg = '';
-        }, 3000)
-      }
-    );
+  async registerUser() {
+    await this.authService.registerUser(this.idToken).toPromise()
+      .then(this.authObserver.next)
+      .catch(this.authObserver.error);
   }
 
-  // logout user profile
-  logoutUser(prompt: boolean = false): void {
+  // shows a prompt to user and gets confirmation
+  getUserConfirmation(prompt: string): boolean {
+    let confirmation: boolean = window.confirm(prompt);
+    return confirmation;
+  }
+
+  // hide error message after some time
+  autoHideErrorMsg(): void {
+    setTimeout(() => {
+      this.errorMsg = '';
+    }, 3000);
+  }
+
+  logoutUser(showPrompt: boolean = false): void {
     // show prompt if required and wait for user confirmation
-    if (prompt) {
-      let confirmation = window.confirm("Are you sure you want to logout?");
-      if (!confirmation) {
+    if (showPrompt) {
+      if (!this.getUserConfirmation("Are yo sure you want to log out?")) {
         return;
-      }  
+      }
     } 
 
     // logs the user google account out
